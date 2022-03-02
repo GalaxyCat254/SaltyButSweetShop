14766a3aa7ad87faf752774886e533a7ffc412a6
Galaxycat254 <80495417+GalaxyCat254@users.noreply.github.com>
2022-03-01 18:41:07 -0800

    Add files via upload

---
+++ cutee.js
@@ -0,0 +1,9 @@
+user_name = localStorage.getItem("user_name");
+console.log(user_name);
+document.getElementById("name").innerHTML = "Welcome " + user_name + "!";
+
+function logout() {
+    localStorage.removeItem("user_name");
+    window.location = "shop.html";
+}
+
